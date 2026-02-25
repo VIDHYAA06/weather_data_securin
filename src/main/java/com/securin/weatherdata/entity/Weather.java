70c6ff2fcff2e5586768272ccc6952b52309fccd
Vidhya Shree <717823s161@kce.ac.in>
2026-02-25 14:24:49 +0530

    commit

--- src/main/java/com/securin/weatherdata/entity/Weather.java
+++ src/main/java/com/securin/weatherdata/entity/Weather.java
@@ -11,28 +11,74 @@ import java.time.LocalDateTime;
 @Setter
 @Document(collection = "weather")
 public class Weather {
-
-    @Id
+    
+ @Id
     private String id;
 
     private LocalDateTime dateTime;
     private String condition;
-    private Double temperature;
+    private Double dewPoint;
+    private Integer fog;
+    private Integer hail;
+    private Double heatIndex;
     private Double humidity;
+    private Double precipitation;
     private Double pressure;
-
-    public Weather() {}
-
+    private Integer rain;
+    private Integer snow;
+    private Double temperature;
+    private Integer thunder;
+    private Integer tornado;
+    private Double visibility;
+    private Integer windDirectionDegrees;
+    private String windDirection;
+    private Double windGust;
+    private Double windChill;
+    private Double windSpeed;
+    public Weather() {
+    }
     public Weather(LocalDateTime dateTime,
                    String condition,
-                   Double temperature,
+                   Double dewPoint,
+                   Integer fog,
+                   Integer hail,
+                   Double heatIndex,
                    Double humidity,
-                   Double pressure) {
+                   Double precipitation,
+                   Double pressure,
+                   Integer rain,
+                   Integer snow,
+                   Double temperature,
+                   Integer thunder,
+                   Integer tornado,
+                   Double visibility,
+                   Integer windDirectionDegrees,
+                   String windDirection,
+                   Double windGust,
+                   Double windChill,
+                   Double windSpeed) {
 
         this.dateTime = dateTime;
         this.condition = condition;
-        this.temperature = temperature;
+        this.dewPoint = dewPoint;
+        this.fog = fog;
+        this.hail = hail;
+        this.heatIndex = heatIndex;
         this.humidity = humidity;
+        this.precipitation = precipitation;
         this.pressure = pressure;
+        this.rain = rain;
+        this.snow = snow;
+        this.temperature = temperature;
+        this.thunder = thunder;
+        this.tornado = tornado;
+        this.visibility = visibility;
+        this.windDirectionDegrees = windDirectionDegrees;
+        this.windDirection = windDirection;
+        this.windGust = windGust;
+        this.windChill = windChill;
+        this.windSpeed = windSpeed;
     }
+
+
 }
